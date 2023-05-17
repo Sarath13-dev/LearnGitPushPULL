d7bfb94f029f081595a57e7aa4b041d29481de70
Sarath13-dev <85007746+Sarath13-dev@users.noreply.github.com>
2023-05-17 08:22:34 +0530

    Update Sample.java

--- src/org/login/Sample.java
+++ src/org/login/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Learn Git");
 		System.out.println("Hi How are you");
+		System.out.println("Hi How are you");
 		
 	}
 
